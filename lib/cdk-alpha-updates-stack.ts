11ebe873b079eeefbd6090f25841275257d78aaf
Torben Neufeldt <torbenneufeldt@gmail.com>
2023-08-15 09:10:48 +0200

    cdk init

---
+++ lib/cdk-alpha-updates-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkAlphaUpdatesStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkAlphaUpdatesQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
